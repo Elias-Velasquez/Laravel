aff4b518093ee6ef8d9fa160add34bf0a119fc60
eliasv <eliasv.devel@gmail.com>
2023-08-16 15:12:21 -0500

    ejercicios

---
+++ 11-Ejercicios/ejercicio1.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Ejercicio1
+
+Crear dos variables "pais" y continente y mostrar su valor por pantalla
+Poner en un comentario que tipo de dato tienen. 
+*/
+
+$pais = "Argentina"; //string
+$continente = "America"; //string
+
+echo "<h1>$pais - $continente</h1>";
+
---
+++ 11-Ejercicios/ejercicio2.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Ejercicio 2. Escribir un script en PHP que nos muestre por pantalla todos los numeros pares que hay
+*/
+
+for ($i = 1; $i <=100 ; $i++){
+
+    if($i%2 == 0){
+    echo $i." <br/>";
+}
+}
